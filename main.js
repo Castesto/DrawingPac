963cd7c750ec91902ab9bfc6c0b3a3600301f370
Castesto <castielhadinheaven@outlook.com>
2020-11-11 17:55:30 +0300

    Add Line sin

--- main.js
+++ main.js
@@ -14,4 +14,22 @@
     drawLine(100, 100, 140, 100, id, context);
     
     drawLine(100, 100, 100, 60, id, context);
+    
+    startX = 100;
+    startY = 100;
+    
+    
+    (function myLoop(i) {
+      setTimeout(function() {
+        let x = Math.round(startX * Math.cos(i / 57.296) + startY * Math.sin(i / 57.296));
+        
+        let y = Math.round(startX * Math.sin(i / 57.296) + startY * Math.cos(i / 57.296));
+        drawLine(20, 20, x, y, id, context);
+        
+        startX = x;
+        startY = y;
+        
+        if (--i) myLoop(i); //  decrement i and call myLoop again if i > 0
+      }, 360)
+    })(100);
  
